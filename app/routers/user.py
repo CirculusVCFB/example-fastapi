fee9d696e459f10db5b426a43aebe1e930eca7df
Frankley Bwalya <CirculusVCFB>
2022-03-10 01:57:07 -0500

    working on inheritance for models k15

--- app/routers/user.py
+++ app/routers/user.py
@@ -46,8 +46,7 @@ def get_ticket(db: Session = Depends(get_db)):
 
 @router.post("/tickets/add/{table}", status_code = status.HTTP_201_CREATED,response_model=schemas.Ticket)
 def post_ticket(table: str, ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
-	prize = table.replace('"',"")
-	new_ticket = models.prize(**ticket.dict())
+	new_ticket = models.table.strip("")(**ticket.dict())
 	db.add(new_ticket)
 	db.commit()
 	db.refresh(new_ticket)
