bce833e3c241fa455c1cfb08acab06fc9f041d60
Frankley Bwalya <CirculusVCFB>
2022-03-10 01:49:34 -0500

    working on post ticket through parameter 2

--- app/routers/user.py
+++ app/routers/user.py
@@ -44,16 +44,16 @@ def get_ticket(db: Session = Depends(get_db)):
 	tickets = db.query(models.Prizetablek10).filter(models.Prizetablek10.username == None).first()
 	return tickets
 
-@router.post("/tickets/add/{table}", status_code = status.HTTP_201_CREATED)
+@router.post("/tickets/add/{table}", status_code = status.HTTP_201_CREATED,response_model=schemas.Ticket)
 def post_ticket(table: str, ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
-	prize = table
-	#new_ticket = models.prize(**ticket.dict())
-	#db.add(new_ticket)
-	#db.commit()
-	#db.refresh(new_ticket)
+	prize = table.replace('"',"")
+	new_ticket = models.prize(**ticket.dict())
+	db.add(new_ticket)
+	db.commit()
+	db.refresh(new_ticket)
 
-	#return new_ticket
-	return prize
+	return new_ticket
+	
 
 @router.put("/tickets/change")
 def update_ticket(ticket: schemas.TicketUpdateUser, db: Session = Depends(get_db), response_model=schemas.Ticket):
