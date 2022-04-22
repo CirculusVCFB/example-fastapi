28e01b850dd18ae049ef688acaed3206d9a292dd
Frankley Bwalya <CirculusVCFB>
2022-04-22 11:32:56 -0400

    working on updates for complete ticket xxxx lol

--- app/routers/user.py
+++ app/routers/user.py
@@ -58,8 +58,9 @@ def update_ticket(ticket: schemas.TicketUpdateUser, db: Session = Depends(get_db
 @router.put("/tickets/complete/prizetablek10")
 def update_ticket(ticket: schemas.TicketUpdateUser, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 	tickets = db.query(models.Prizetablek10).filter(models.Prizetablek10.username == "royalty200@gmail.com").first()
+	ticket_to_grab = db.query(models.Prizetablek10).filter(models.Prizetablek10.id == tickets.id )
 	print("The tickets are")
-	print(tickets)
+	print(ticket_to_grab)
 
 
 
